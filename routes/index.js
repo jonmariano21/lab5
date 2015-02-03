aef13a77d3f998ea06b426776becd9460ae89e4f
Jonathan Mariano <jamarian@ucsd.edu>
2015-02-02 17:12:20 -0800

    Act One: not working

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 // Get all of our friend data
+//This loads our JSON db into data. Will be printed to the console.log when you load the page
 var data = require('../data.json');
 
 exports.view = function(req, res){
